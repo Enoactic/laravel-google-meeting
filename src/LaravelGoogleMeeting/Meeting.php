4641b43e4d0d1f1cec80bc874c488f1c63bc7243
Boonyaphat Intasin <aosmosiss-ze@hotmail.com>
2021-05-18 16:48:06 +0700

    - Update functions

--- src/LaravelGoogleMeeting/Meeting.php
+++ src/LaravelGoogleMeeting/Meeting.php
@@ -63,82 +63,7 @@ class Meeting
         $this->client = $client;
     }
 
-    public function getCalendar()
-    {
-        $service = new Google_Service_Calendar($this->client);
-
-        $calendarList = $service->calendarList->listCalendarList();
-
-        $calendars = array();
-        while(true) {
-            foreach ($calendarList->getItems() as $calendarListEntry) {
-                $calendars[] = $calendarListEntry;
-            }
-            $pageToken = $calendarList->getNextPageToken();
-            if ($pageToken) {
-                $calendarList = $service->calendarList->listCalendarList(array('pageToken' => $pageToken));
-            } else {
-                break;
-            }
-        }
-
-        return $calendars;
-    }
-
-    public function createCalendar($name)
-    {
-        $service = new Google_Service_Calendar($this->client);
-
-        $calendar = new Google_Service_Calendar_Calendar();
-        $calendar->setSummary($name);
-        $calendar->setTimeZone('Asia/Bangkok');
-
-        $createdCalendar = $service->calendars->insert($calendar);
-
-        return $createdCalendar->getId();
-    }
-
-    public function deleteCalendar($id)
-    {
-        $service = new Google_Service_Calendar($this->client);
-
-        $service->calendars->delete($id);
-    }
-
-    public function getEvent($id, $start = null, $end = null)
-    {
-        $params = array();
-        if($start != null){
-            $start = new \DateTime($start);
-            $params['timeMin'] = $start->format(\DateTime::RFC3339);
-        }
-        if($end != null){
-            $end = new \DateTime($end);
-            $params['timeMax'] = $end->format(\DateTime::RFC3339);
-        }
-
-        $service = new Google_Service_Calendar($this->client);
-
-        $eventList = $service->events->listEvents($id, $params);
-
-        $events = array();
-        while(true) {
-            foreach ($eventList->getItems() as $eventListEntry) {
-                $events[] = $eventListEntry;
-            }
-            $pageToken = $eventList->getNextPageToken();
-            if ($pageToken) {
-                $params['pageToken'] = $pageToken;
-                $eventList = $service->events->listEvents($id, $params);
-            } else {
-                break;
-            }
-        }
-
-        return $events;
-    }
-
-    public function createEvent($calendarId, $name, $location, $description, $start, $end, $attendees)
+    public function createMeeting($calendarId, $name, $start, $end, $meetingId)
     {
         $start = new \DateTime($start);
         $end = new \DateTime($end);
@@ -147,8 +72,6 @@ class Meeting
 
         $event = new Google_Service_Calendar_Event(array(
             'summary' => $name,
-            'location' => $location,
-            'description' => $description,
             'start' => array(
                 'dateTime' => $start->format(\DateTime::RFC3339),
                 'timeZone' => 'Asia/Bangkok',
@@ -157,25 +80,18 @@ class Meeting
                 'dateTime' => $end->format(\DateTime::RFC3339),
                 'timeZone' => 'Asia/Bangkok',
             ),
-            'attendees' => $attendees,
-            'reminders' => array(
-                'useDefault' => false,
-                'overrides' => array(
-                    array('method' => 'email', 'minutes' => 24 * 60),
-                    array('method' => 'popup', 'minutes' => 10),
-                ),
-            ),
         ));
 
         $event = $service->events->insert($calendarId, $event);
 
-        return $event->id;
-    }
+        $conference = new \Google_Service_Calendar_ConferenceData();
+        $conferenceRequest = new \Google_Service_Calendar_CreateConferenceRequest();
+        $conferenceRequest->setRequestId($meetingId);
+        $conference->setCreateRequest($conferenceRequest);
+        $event->setConferenceData($conference);
 
-    public function deleteEvent($calendarId, $id)
-    {
-        $service = new Google_Service_Calendar($this->client);
+        $event = $service->events->patch($calendarId, $event->id, $event, ['conferenceDataVersion' => 1]);
 
-        $service->events->delete($calendarId, $id);
+        return $event;
     }
 }
